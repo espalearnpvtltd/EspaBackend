007b9f30b391ac2bbc804bfc8d9d60f64fb1c2e9
Omika Raina <omika.raina.nodejs@sarvm.ai>
2025-11-28 13:52:05 +0530

    Fix Cloud Run port and server listen syntax

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
